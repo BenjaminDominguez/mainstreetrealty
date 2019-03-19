c398068886fa146cbd7f630cbac0f25cfb8d8c71
Ben Dominguez <bendominguez011@gmail.com>
2019-03-19 01:47:17 -0400

    changed env vars

--- mainstreet/settings.py
+++ mainstreet/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.urandom(24)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.mainstreetrealtyandassociates.com']
 
 
 # Application definition
